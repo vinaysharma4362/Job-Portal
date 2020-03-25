1bba2bfdc051820f0034cb4ccce5e84c46b735eb
Zeel Bhavsar <zeel.bhavsar@bacancytechnology.com>
2020-03-25 17:27:49 +0530

    Added Association, Change in routes

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Resume < ApplicationRecord
   belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :companies
+  resources :companies do
+    resources :job_posts
+  end
   root 'dashboards#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
