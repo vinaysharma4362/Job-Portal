f898eacf2e4373d632fcc47d6689616b27990203
Zeel Bhavsar <zeel.bhavsar@bacancytechnology.com>
2020-03-30 19:31:39 +0530

    Added Job Search functionality

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,18 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: ["popper.js", "default"],
+    'windows.jQuery': 'jquery'
+  })
+)
+const aliasConfig = {
+    'jquery': 'jquery/src/jquery',
+    'jquery-ui': 'jquery-ui-dist/jquery-ui.js'
 
+};
+
+environment.config.set('resolve.alias', aliasConfig);
 module.exports = environment
