428f1fe7e2438a3764f39ec4fa94d12e067bd23b
Zeel Bhavsar <zeel.bhavsar@bacancytechnology.com>
2020-04-01 17:47:03 +0530

    Change in routes

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,6 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'job_posts/search'
+  post 'job_posts/search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
