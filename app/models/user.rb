f8c7bd4ee82dc2efc8c892db66f3c4e3e16df592
Zeel Bhavsar <zeel.bhavsar@bacancytechnology.com>
2020-04-03 19:04:03 +0530

    Add rolify gem and create admin using seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 # User model
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :resume
