0e74a39a4621a7b6e49eb08d009775a099c00707
Zeel Bhavsar <zeel.bhavsar@bacancytechnology.com>
2020-04-09 16:35:52 +0530

    Add admin modules edit/delete companies, jobseekers and job posts functionality

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,11 +2,44 @@
 
 # companies controller
 class CompaniesController < ApplicationController
+  before_action :find_company, only: %i[edit update destroy] 
   def index
     @companies = Company.all
   end
 
   def show
     @company = Company.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @company.update(company_params)
+      redirect_to admins_companies_path,
+                  notice: 'Company was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @company.destroy
+    redirect_to admins_companies_path, notice: 'Job Post was successfully destroyed.'
+  end
+
+  private
+
+  def find_company
+    @company = Company.find_by(id: params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list
+  # through.
+  def company_params
+    params.require(:company).permit(:title, :about, :address,
+                                     :city, :state, :country,
+                                     :pincode, :contact_no, :website,
+                                     :status)
   end
 end
--- app/controllers/companies_dashboards_controller.rb
+++ app/controllers/companies_dashboards_controller.rb
@@ -4,9 +4,17 @@
 class CompaniesDashboardsController < ApplicationController
   before_action :authenticate_company!
   def index
+<<<<<<< Updated upstream
     redirect_to company_job_posts_path(current_company.id) if current_company
   end
 
+=======
+    if current_company
+      redirect_to company_job_posts_path(current_company.id)
+    end
+  end
+ 
+>>>>>>> Stashed changes
   def company_show
     @company = Company.all
   end
